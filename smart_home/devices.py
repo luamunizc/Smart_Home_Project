ce7a12fede214210cc8fd6315437d2f7bcbd3003
luamunizc <luyaram@gmail.com>
2025-09-03 07:50:03 -0300

    initial push

---
+++ smart_home/devices.py
@@ -0,0 +1,34 @@
+class Devices:
+    def __init__(self):
+        pass
+
+
+class Lamp(Devices):
+    def __init__(self):
+        super().__init__()
+
+
+class Door(Devices):
+    def __init__(self):
+        super().__init__()
+
+
+class Switch(Devices):
+    def __init__(self):
+        super().__init__()
+
+
+class Alarm(Devices):
+    def __init__(self):
+        super().__init__()
+
+
+
+class Cam(Devices):
+    def __init__(self):
+        super().__init__()
+
+
+class CatFeeder(Devices):
+    def __init__(self):
+        super().__init__()
